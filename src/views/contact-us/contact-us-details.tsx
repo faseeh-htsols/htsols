52423cb6a5412a35e96e24e7f471605df43d0753
Programmer <halaltechsolutions@gmail.com>
2025-12-22 14:29:53 +0500

    padding additions

--- src/views/contact-us/contact-us-details.tsx
+++ src/views/contact-us/contact-us-details.tsx
@@ -22,7 +22,7 @@ const data = [
 ];
 const ContactUsDetails = () => {
   return (
-    <div className="bg-black text-white pb-10">
+    <div className="bg-black text-white pb-24">
       <Container>
         <div className="flex flex-col gap-5 mb-7">
           <HeadingTwo className="text-center">Contact us details</HeadingTwo>
